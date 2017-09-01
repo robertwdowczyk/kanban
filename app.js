c34c38275ea751c4c410fb9879b8bf44eba0a82d
robertw <r.wdowczyk@gmail.com>
2017-09-01 15:40:25 +0200

    kanban

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': 1701,
+  'X-Auth-Token': '7812dddf4472079ccf17fe2e058328b7'
+};
+$.ajaxSetup({
+    headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.createCard(card);
+    })
+}
